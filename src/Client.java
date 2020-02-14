2f08f8a8d070902af2455b34c2d90444abcf5e3b
Timo Schmidt <schmidt@timo.email>
2020-02-14 13:33:57 +0100

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        try {
+            int port = 8000;
+            Socket s = new Socket("localhost", port);
+            System.out.println("Client is connected to port " + port + " ...");
+
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String incoming = "";
+            String outgoing = "";
+
+            while (!incoming.equals("quit")) {
+
+                System.out.print("> ");
+
+                incoming = br.readLine();
+                dos.writeUTF(incoming);
+                dos.flush();
+
+                outgoing = dis.readUTF();
+                System.out.println("Server: " + outgoing);
+
+            }
+
+            dos.close();
+            s.close();
+
+        }
+
+        catch (IOException e) {
+            System.out.println("IOException"); // handle exception
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/Server.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+        try {
+            int port = 8000;
+            ServerSocket ss = new ServerSocket(port);
+            System.out.println("Server is connected to port " + port + " ...");
+
+            Socket s = ss.accept(); // establish connection
+
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String incoming = "";
+            String outgoing = "";
+
+            while (!incoming.equals("quit")) {
+
+                incoming = dis.readUTF();
+                System.out.println("Client: " + incoming);
+
+                System.out.print("> ");
+
+                outgoing = br.readLine();
+                dos.writeUTF(outgoing);
+                dos.flush();
+
+            }
+
+            dis.close();
+            s.close();
+            ss.close();
+
+        }
+
+        catch (IOException e) {
+            System.out.println("IOException"); // handle exception
+            e.printStackTrace();
+        }
+
+    }
+
+}
